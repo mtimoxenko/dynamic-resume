c4db2c40d19a47e8d4377c8c7f6b5a37f0382d26
fr3m3n <mtimochenko@tutanota.com>
2023-11-11 01:43:12 -0300

    ec2 -> nginx

---
+++ IaC/provisioning/main.tf
@@ -0,0 +1,27 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-0f34c5ae932e6f0e4"
+  instance_type = "t2.micro"
+
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install -y nginx
+              echo "<h1>Hello from Terraform</h1>" > /usr/share/nginx/html/index.html
+              systemctl start nginx
+              systemctl enable nginx
+              EOF
+
+  tags = {
+    Name = "nginx-ec2-timo"
+  }
+}
+
+
+output "ec2_public_ip" {
+  description = "Public IP of the EC2 instance"
+  value       = aws_instance.web.public_ip
+}

a56eb557680022ccb6bf608e357ad8fef8ddc725
fr3m3n <mtimochenko@tutanota.com>
2023-11-11 18:59:17 -0300

    DinD approach [test]

--- IaC/provisioning/backend.tf
+++ IaC/provisioning/backend.tf
@@ -3,7 +3,7 @@ terraform {
     address        = "https://gitlab.com/api/v4/projects/51944352/terraform/state/default"
     lock_address   = "https://gitlab.com/api/v4/projects/51944352/terraform/state/default/lock"
     unlock_address = "https://gitlab.com/api/v4/projects/51944352/terraform/state/default/lock"
-    username       = "gitlab-ci-token"
-    password       = "${CI_JOB_TOKEN}"
+    # username       = "gitlab-ci-token"
+    # password       = "${CI_JOB_TOKEN}"
   }
 }

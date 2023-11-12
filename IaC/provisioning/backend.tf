62901b45db79c8a7905622b288ca23c94651bf1f
fr3m3n <mtimochenko@tutanota.com>
2023-11-12 18:46:27 -0300

    terra test

--- IaC/provisioning/backend.tf
+++ IaC/provisioning/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "http" {
-    address        = "https://gitlab.com/api/v4/projects/51944352/terraform/state/default"
-    lock_address   = "https://gitlab.com/api/v4/projects/51944352/terraform/state/default/lock"
-    unlock_address = "https://gitlab.com/api/v4/projects/51944352/terraform/state/default/lock"
-    # username       = "gitlab-ci-token"
-    # password       = "${CI_JOB_TOKEN}"
+    # address        = "https://gitlab.com/api/v4/projects/51944352/terraform/state/default"
+    # lock_address   = "https://gitlab.com/api/v4/projects/51944352/terraform/state/default/lock"
+    # unlock_address = "https://gitlab.com/api/v4/projects/51944352/terraform/state/default/lock"
+    # # username       = "gitlab-ci-token"
+    # # password       = "${CI_JOB_TOKEN}"
   }
 }

c514ea14a9a2c7994a9b8b8e965730fd83de6980
fr3m3n <mtimochenko@tutanota.com>
2023-11-14 18:36:46 -0300

    [ansible] playbook test [+] key pair at launch

--- IaC/provisioning/main.tf
+++ IaC/provisioning/main.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "web" {
   ami           = "ami-0f34c5ae932e6f0e4"
   instance_type = "t2.micro"
+  key_name      = "aws_key_dh"
 
   user_data = <<-EOF
               #!/bin/bash
